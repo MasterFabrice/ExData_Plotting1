6a6e56f08bc1d9e1fffb499372e21d097b151e8a
MasterFabrice <fabrice.vouillamoz@icloud.com>
2017-03-26 19:56:47 +0200

    Update Assignement_04_01_Plot4.R

--- Assignement_04_01_Plot4.R
+++ Assignement_04_01_Plot4.R
@@ -19,7 +19,7 @@ points(dat1$DateTime, dat1$Sub_metering_2, col = "red", type = "l")
 points(dat1$DateTime, dat1$Sub_metering_3, col = "blue", type = "l")
 legend("topright",lty=c(1,1,1),col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=0.5, box.lty=0)
 plot(dat1$DateTime, dat1$Global_reactive_power,  type = "l", xlab ="datetime", ylab ="Global_reactive_power")
-dev.copy(png, file = "plot4.png", width=480, height=480)
+dev.copy(png, file = "plot41.png", width=480, height=480)
 dev.off()
 
 
